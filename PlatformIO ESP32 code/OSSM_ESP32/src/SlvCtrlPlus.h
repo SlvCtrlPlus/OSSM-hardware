8df8e83bd567403acd4226352824916f6c4a41c2
heavyrubberslave <pascal@heavyrubberslave.com>
2023-03-18 01:04:02 +0100

    Fix scope of class members and variables

--- PlatformIO ESP32 code/OSSM_ESP32/src/SlvCtrlPlus.h
+++ PlatformIO ESP32 code/OSSM_ESP32/src/SlvCtrlPlus.h
@@ -7,13 +7,16 @@
 class SlvCtrlPlus
 {
    public:
+    SlvCtrlPlus(OSSM* ossm);
+    void comm_setup();
+    void comm_loop();
+
+   private:
     char serial_command_buffer[32];
     SerialCommands serialCommands;
     static OSSM* ossm;
 
-    SlvCtrlPlus(OSSM* ossm);
-    void comm_setup();
-    void comm_loop();
+
     void static serial_printf(Stream *serial, const char* format, ...);
 
     void static commandUnrecognized(SerialCommands* sender, const char* cmd);
